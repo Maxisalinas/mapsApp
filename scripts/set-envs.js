dbbd484702a911d624c3f9d349f945bc4d8afe42
Maximiliano Salinas <maxiezequielsalinas7@gmail.com>
2024-05-26 18:28:03 -0300

    mapsApp

---
+++ scripts/set-envs.js
@@ -0,0 +1,17 @@
+const { mkdirSync, writeFileSync } = require('fs');
+
+require('dotenv').config()
+
+const targetPath= './src/environments/environment'
+
+const envFileContent = `
+    export const environment = {
+    
+        mapbox_key: "${ process.env['MAPBOX_KEY'] }"
+
+    }; 
+`;
+
+
+mkdirSync('./src/environments', { recursive: true } );
+writeFileSync( targetPath, envFileContent );
